de6e941b805b6d2904c695665eb914c510ecc24a
Patrycja StypuÅła <patistypula90@onet.eu>
2020-10-15 17:08:55 +0200

    first commit

---
+++ src/main/java/pl/coderslab/tanitabody/TanitabodyApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.tanitabody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanitabodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TanitabodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/tanitabody/TanitabodyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.tanitabody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanitabodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
