251d4af07d9cda38611ad956b9870381bc49ef61
Patrycja StypuÅła <patistypula90@onet.eu>
2021-01-09 11:21:29 +0100

    final modyfications

--- src/main/java/pl/coderslab/tanitabody/TanitaBodyApplication.java
+++ src/main/java/pl/coderslab/tanitabody/TanitaBodyApplication.java
@@ -11,21 +11,4 @@ public class TanitaBodyApplication {
         SpringApplication.run(TanitaBodyApplication.class, args);
     }
 
-//    private static final Logger logger = Logger.getLogger(TanitaBodyApplication.class);
-//
-//    public static void main(String[]args) {
-//        System.out.println(getLocalCurrentDate());
-//    }
-//
-//    private static String getLocalCurrentDate() {
-//
-//        if (logger.isDebugEnabled()) {
-//            logger.debug("getLocalCurrentDate() is executed!");
-//        }
-//
-//        LocalDate date = new LocalDate();
-//        return date.toString();
-//
-//    }
-
 }
